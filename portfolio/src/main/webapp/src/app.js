45ddcf2540848c410b196ea9d72032632257847c
Cody Rivera <cjrivera1@crimson.ua.edu>
2020-06-01 10:27:15 -0500

    Fix style according to HTML, CSS, and JavaScript Style Guides

--- portfolio/src/main/webapp/src/app.js
+++ portfolio/src/main/webapp/src/app.js
@@ -1,3 +1,20 @@
-import {MDCRipple} from '@material/ripple/index';
+// Copyright 2020 Cody Rivera
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+/**
+ * @fileoverview Javascript for home page. Currently this file
+ * does nothing, as the home page is static.
+ * @package
+ */
+
